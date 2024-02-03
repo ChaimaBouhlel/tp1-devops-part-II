1d82a8a642ffc0700dac1edcc668d3333aa629af
Chaima Bouhlel <chaima.bouhlel@insat.u-carthage.tn>
2024-02-03 23:25:59 +0100

    change product file

--- product.py
+++ product.py
@@ -1,10 +1,14 @@
 def calculate_product(arr):
+    # Calculate the product of array elements
     result = 1
     for num in arr:
+        # Multiply each element with the result
         result *= num
     return result
+print("This is product.py")
 
 def main():
+        # Get input from the user
         input_str = input("Enter the array elements separated by spaces: ")
         input_arr = list(map(int, input_str.split()))
         
@@ -16,5 +20,6 @@ def main():
             product = calculate_product(input_arr)
             print("Product of array elements:", product)
 
+print("This is product.py")
 if __name__ == "__main__":
     main()
